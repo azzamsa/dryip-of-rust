aaba2f41644309ce35533494cf66b79dbc456cb8
azzamsa <me@azzamsa.com>
2020-09-21 02:55:32 +0700

    doc: add initial doc

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 fn main() {
     println!("=== 30 Seconds of Rust ===");
-    println!(
-        "hex_to_rgb: FFA501 to {:?}",
-        hex_to_rgb("FFA501")
-    );
+    println!("hex_to_rgb: FFA501 to {:?}", hex_to_rgb("FFA501"));
 }
 
-fn hex_to_rgb(hex: &str) -> Vec<i64>{
+/// convert HEX into RGB value
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// assert_eq!(vec![255, 165, 1], hex_to_rgb(&"FFA501"));
+/// ```
+fn hex_to_rgb(hex: &str) -> Vec<i64> {
     let to_base16 = |x| i64::from_str_radix(x, 16);
     // MAYBE can I collect as tuple directly?
-    [0,2,4].iter().map(|&x| to_base16(&hex[x..x+2]).unwrap()).collect::<Vec<_>>()
+    [0, 2, 4]
+        .iter()
+        .map(|&x| to_base16(&hex[x..x + 2]).unwrap())
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
