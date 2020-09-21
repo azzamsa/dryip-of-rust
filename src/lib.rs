c28216414465bad6758bbc309d1465c95865b7fb
azzamsa <me@azzamsa.com>
2020-09-21 11:04:30 +0700

    refactor: move all code to lib.rs
    Currently Cargo is not able to run doctest on bin-project.

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+/// convert HEX into RGB value
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// assert_eq!(vec![255, 165, 1], thirtyseconds::hex_to_rgb(&"FFA501"));
+/// ```
+pub fn hex_to_rgb(hex: &str) -> Vec<i64> {
+    let to_base16 = |x| i64::from_str_radix(x, 16);
+    // MAYBE can I collect as tuple directly?
+    [0, 2, 4]
+        .iter()
+        .map(|&x| to_base16(&hex[x..x + 2]).unwrap())
+        .collect::<Vec<_>>()
+}
+
+/// convert RGB into HEX value
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// assert_eq!("FFA501", thirtyseconds::rgb_to_hex(255, 165, 1));
+/// ```
+pub fn rgb_to_hex(r: i32, g: i32, b: i32) -> String {
+    format!("{:02X}{:02X}{:02X}", r, g, b)
+}
+
+/// check if all value in given list are equal
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// assert_eq!(true, thirtyseconds::all_equal(vec![2, 2, 2]));
+/// ```
+pub fn all_equal(lst: Vec<u32>) -> bool {
+    // FIX why the python version use lst[1:] == lst[:-1]?
+    &lst[..] == &lst[..]
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hex_to_rgb() {
+        assert_eq!(vec![255, 165, 1], hex_to_rgb(&"FFA501"));
+    }
+    #[test]
+    fn test_rgb_to_hex() {
+        assert_eq!("FFA501", rgb_to_hex(255, 165, 1));
+    }
+    #[test]
+    fn test_all_equal() {
+        assert_eq!(true, all_equal(vec![2, 2, 2]));
+    }
+}
