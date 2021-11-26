89352dba1536a7eae1eba0c70e0c9ee073ac95fe
azzamsa <me@azzamsa.com>
2021-11-26 21:14:36 +0700

    doc: use ferris for logo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![doc(html_logo_url = "https://rustacean.net/assets/cuddlyferris.png")]
+#![doc(html_favicon_url = "https://rustacean.net/assets/cuddlyferris.png")]
+
 //! # 30 Seconds of Rust 🦀
 //!
 //! Hello, and welcome to the 30 Seconds of Rust 🦀 website! 👋
