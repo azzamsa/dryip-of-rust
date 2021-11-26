cea569727eefa5dad9d3c8ccb9011b0dff5d06af
azzamsa <me@azzamsa.com>
2021-11-26 13:30:38 +0700

    docs: add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # 30 Seconds of Rust 🦀
+//!
+//! Hello, and welcome to the 30 Seconds of Rust 🦀 website! 👋
+//!
+//! - Each function categorized into appropriate module.
+//! - Use the search bar to find the function.
+//! - Click the `[SRC]` button at the top to see the complete source code.
+//!
+
 pub mod arrays;
 pub mod maths;
 pub mod strings;
